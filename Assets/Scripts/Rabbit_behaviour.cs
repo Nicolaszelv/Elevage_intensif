e602416fbd80aef29def7c17b420ff42837c3d3d
Nicolaszelv <nicolas.zelv@gmail.com>
2020-10-19 20:29:34 +0200

    Accouplement si horny

--- Assets/Scripts/Rabbit_behaviour.cs
+++ Assets/Scripts/Rabbit_behaviour.cs
@@ -13,11 +13,28 @@ public class Rabbit_behaviour : MonoBehaviour
     public float maxX;
     public float minY;
     public float maxY;
+    public bool horny_male;
+    public bool horny_femelle;
+    Renderer rend;
+    Color Rouge = Color.red;
 
     void Start()
     {
         waitTime = startWaitTime;
         moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        rend = GetComponent<Renderer>();
+
+        if (gameObject.tag == "Male") {
+            horny_male = true;
+            horny_femelle = false;
+        }
+
+        if (gameObject.tag == "Femelle") {
+            horny_male = false;
+            horny_femelle = true;
+        }
+        
+        
     }
 
     // Update is called once per frame
@@ -34,12 +51,28 @@ public class Rabbit_behaviour : MonoBehaviour
                 waitTime -= Time.deltaTime;
             }
         }
+        
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Femelle" & gameObject.tag == "Male")
-        {
-            Debug.Log("baise");
-        }
+        if (other.gameObject.tag == "Femelle" & gameObject.tag == "Male" & other.gameObject.GetComponent<Rabbit_behaviour>().horny_femelle == true & horny_male == true) // SI LAPIN RENCONTRE LAPINE
+            {
+                //speed = 1;
+                //other.gameObject.GetComponent<Rabbit_behaviour>().speed = 1;
+                Destroy(gameObject);
+                Destroy(other.gameObject);
+                horny_male = false;
+                Debug.Log("LAPIN RENCONTRE LAPINE");
+            }
+
+        if (other.gameObject.tag == "Male" & gameObject.tag == "Femelle" & other.gameObject.GetComponent<Rabbit_behaviour>().horny_male == true & horny_femelle == true) // SI LAPINE RENCONTRE LAPINE
+            {
+                horny_femelle = false;
+                Destroy(gameObject);
+                Destroy(other.gameObject);
+                //other.gameObject.GetComponent<Rabbit_behaviour>().speed = 1;
+                //speed = 1;
+                Debug.Log("LAPINE RENCONTRE LAPIN");
+            }
     }
 }
