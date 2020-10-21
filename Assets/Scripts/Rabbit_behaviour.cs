7bbe447ea875aca0e385675133cd42b9d098052b
Nicolaszelv <nicolas.zelv@gmail.com>
2020-10-21 12:05:58 +0200

    Lifetime and growing size

--- Assets/Scripts/Rabbit_behaviour.cs
+++ Assets/Scripts/Rabbit_behaviour.cs
@@ -7,7 +7,9 @@ public class Rabbit_behaviour : MonoBehaviour
     
     public float speed;
     private float waitTime;
-    public float startWaitTime;
+    private float startWaitTime;
+    public float waitMin;
+    public float waitMax;
     public Transform moveSpot;
     public float minX;
     public float maxX;
@@ -16,11 +18,20 @@ public class Rabbit_behaviour : MonoBehaviour
     public bool horny_male;
     public bool horny_femelle;
     
-    private GameObject go_male;
-    private GameObject go_femelle;
+    public float lifetime;
+    public float random_death_min;
+    public float random_death_max;
+    private float deathtime;
 
-    private SpriteRenderer sr_male;
-    private SpriteRenderer sr_femelle;
+    public float size;
+    public float grow_speed;
+    public float random_size_min;
+    public float random_size_max;
+    private float final_size;
+
+    public int nb_babies_min;
+    public int nb_babies_max;
+    private int nb_babies;
 
     public string[] Sexe = new string[2];
     private int randomIndex;
@@ -31,16 +42,17 @@ public class Rabbit_behaviour : MonoBehaviour
 
     void Start()
     {
+        startWaitTime = Random.Range(waitMin, waitMax+1);
         waitTime = startWaitTime;
         moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
 
-        /* go_male = GameObject.FindWithTag ("Male");
-        sr_male = go_male.GetComponent<SpriteRenderer>();
+        lifetime = 0;
+        deathtime = Random.Range(random_death_min, random_death_max);
 
-        go_femelle = GameObject.FindWithTag ("Femelle");
-        sr_femelle = go_femelle.GetComponent<SpriteRenderer>();
-        transform.position = new Vector3(0.0f, -2.0f, 0.0f); */
+        size = 0;
+        final_size = Random.Range(random_size_min, random_size_max);
 
+        nb_babies = Random.Range(nb_babies_min, nb_babies_max);
 
         if (gameObject.tag == "Male") {
             horny_male = true;
@@ -59,6 +71,20 @@ public class Rabbit_behaviour : MonoBehaviour
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+        
+        lifetime += Time.deltaTime;
+
+        if(lifetime >= deathtime){
+            Destroy(gameObject);
+            Destroy(Move_Spot);
+        }
+
+        size += Time.deltaTime * grow_speed;
+        gameObject.GetComponent<Transform>().localScale = new Vector3 (size, size, size);
+
+        if(size >= final_size){
+            size = final_size;
+        }
 
         if(Vector2.Distance(transform.position, moveSpot.position) < 0.2f){
             if(waitTime <= 0) {
@@ -78,13 +104,20 @@ public class Rabbit_behaviour : MonoBehaviour
                 speed = 1;
                 other.gameObject.GetComponent<Rabbit_behaviour>().speed = 1;
 
-                Transform spot = Instantiate(Move_Spot, gameObject.transform.position, Quaternion.identity).transform;
-                Transform lapin_n = Instantiate(Lapin_neutre, gameObject.transform.position, Quaternion.identity).transform;
                 
-                lapin_n.GetComponent<Rabbit_behaviour>().moveSpot = spot;
-                lapin_n.GetComponent<Rabbit_behaviour>().speed = 8;
-                lapin_n.GetComponent<Transform>().localScale = new Vector3 (0.25f, 0.25f, 0.25f);
-                lapin_n.GetComponent<SpriteRenderer>().color = Color.Lerp(gameObject.GetComponent<SpriteRenderer>().color, other.gameObject.GetComponent<SpriteRenderer>().color,  Mathf.PingPong(Time.time, 1));
+
+                for (int i = 0; i < nb_babies; i++)
+                    {
+                        Transform spot = Instantiate(Move_Spot, gameObject.transform.position, Quaternion.identity).transform;
+                        Transform lapin_n = Instantiate(Lapin_neutre, gameObject.transform.position, Quaternion.identity).transform;
+                        //Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+                        lapin_n.GetComponent<Rabbit_behaviour>().moveSpot = spot;
+                        lapin_n.GetComponent<Rabbit_behaviour>().speed = 8;
+                        lapin_n.GetComponent<SpriteRenderer>().color = Color.Lerp(gameObject.GetComponent<SpriteRenderer>().color, other.gameObject.GetComponent<SpriteRenderer>().color,  Random.Range(0.2f, 0.8f));
+                        Debug.Log("new babies !");
+                    }
+
+                
 
                 //randomIndex = Random.Range (0, 2);
                 //string randomTag = Sexe[randomIndex];
